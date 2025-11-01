c24c3d2702deb7f5a906ecc4205267ee0910265e
Bigfoot71 <victor.lejuez@protonmail.ch>
2025-11-01 17:11:23 +0100

    move global states definitions

---
+++ source/INX_GlobalState.cpp
@@ -0,0 +1,11 @@
+#include "./INX_GlobalState.hpp"
+
+// ============================================================================
+// GLOBAL STATES
+// ============================================================================
+
+INX_DisplayState  INX_Display{};
+INX_AudioState    INX_Audio{};
+INX_KeyboardState INX_Keyboard{};
+INX_MouseState    INX_Mouse{};
+INX_FrameState    INX_Frame{};
--- source/NX_Init.cpp
+++ source/NX_Init.cpp
@@ -33,12 +33,6 @@
 // GLOBAL STATES (NOTE: Will be withdrawn eventually)
 // ============================================================================
 
-INX_DisplayState  INX_Display{};
-INX_AudioState    INX_Audio{};
-INX_KeyboardState INX_Keyboard{};
-INX_MouseState    INX_Mouse{};
-INX_FrameState    INX_Frame{};
-
 std::unique_ptr<NX_RenderState> gRender;
 
 // ============================================================================
